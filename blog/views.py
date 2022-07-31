df1c4e96075788ac8891000a3a9cc93315b146a9
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-07-31 19:36:45 +0400

    adding dictinory with data and put that data dynamicaly in page

--- blog/views.py
+++ blog/views.py
@@ -2,36 +2,85 @@ from django.shortcuts import render
 from datetime import date
 
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Movsesyan",
-        "data": date(2022, 7, 31),
+        "author": "Maximilian",
+        "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
-        "excert": "There's nothing like the views you get when hiking in the mountain! And I wasn't even prepared for what happend whilst I was enjoying the views",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum deleniti, 
-            eaque sed et neque in, provident tempore dicta, ipsa sint quaerat!
-            Asperiores tenetur quae est iusto enim eum amet atque.
-            
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum deleniti, 
-            eaque sed et neque in, provident tempore dicta, ipsa sint quaerat!
-            Asperiores tenetur quae est iusto enim eum amet atque.
-            
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum deleniti, 
-            eaque sed et neque in, provident tempore dicta, ipsa sint quaerat!
-            Asperiores tenetur quae est iusto enim eum amet atque.
-        
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
         """
     }
 ]
+
+
+def get_data(post):
+    return post["date"]
 
 # Create your views here.
 
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_data)
+    latest_post = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_post
+    })
 
 
 def posts(request):
