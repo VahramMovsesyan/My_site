9b9dcccec02881d8d479abf83a98bad482fbd046
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-07-31 16:47:29 +0400

    Adding Dummy Data to the 'Views' File

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from datetime import date
+
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Movsesyan",
+        "data": date(2022, 7, 31),
+        "title": "Mountain Hiking",
+        "excert": "There's nothing like the views you get when hiking in the mountain! And I wasn't even prepared for what happend whilst I was enjoying the views",
+        "content": """
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum deleniti, 
+            eaque sed et neque in, provident tempore dicta, ipsa sint quaerat!
+            Asperiores tenetur quae est iusto enim eum amet atque.
+            
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum deleniti, 
+            eaque sed et neque in, provident tempore dicta, ipsa sint quaerat!
+            Asperiores tenetur quae est iusto enim eum amet atque.
+            
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum deleniti, 
+            eaque sed et neque in, provident tempore dicta, ipsa sint quaerat!
+            Asperiores tenetur quae est iusto enim eum amet atque.
+        
+        """
+    }
+]
 
 # Create your views here.
 
