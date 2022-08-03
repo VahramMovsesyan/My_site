36902b8ceb78bf03795b850baf6199cc1895ce0e
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-03 05:10:17 +0400

    update models & migrations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     # creating fild (colums)
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
     #Django will automatically create the ID column with autoincrement
-    
+
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
