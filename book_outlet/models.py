e52f96b7a71947d8d8881e2620fec0ac3091b8b4
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-03 04:03:57 +0400

    creating tables, making firts migration

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    # creating fild (colums)
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
+    #Django will automatically create the ID column with autoincrement
+    
