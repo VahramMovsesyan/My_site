d8f56a60fe7ebc9b647bb92f91cffae97a72f461
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-04 18:44:08 +0400

    Configuring Model Fields

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -12,7 +12,8 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, 
+                            editable=False ,null=False, db_index=True)
     #Django will automatically create the ID column with autoincrement
 
     def get_absolute_url(self):
