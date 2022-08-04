6955a8eea29d76a5570a9f9a6306ff207f19258b
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-05 02:56:53 +0400

    Adding a one-to-many Relation & Migrations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.forms import CharField
 from django.urls import reverse
 
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
+
 
 class Book(models.Model):
     # creating fild (colums)
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)  
+    # CASCADE tells Django and SQL that if a author should be deleted any related book also should be deleted
+    
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
-    #Django will automatically create the ID column with autoincrement
+    # Django will automatically create the ID column with autoincrement
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
