fe9c8b1d3227c999c886d1bf572a585125512bd2
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-08 23:25:47 +0400

    update

--- blog/views.py
+++ blog/views.py
@@ -88,6 +88,6 @@ def posts(request):
 
 def post_detail(request, slug):
     identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html",{
+    return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
