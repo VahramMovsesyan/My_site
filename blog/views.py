5eb4f6288ae25cfe704aae4ad9b86df775a75c09
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-24 01:15:47 +0400

    Outputting Comments

--- blog/views.py
+++ blog/views.py
@@ -49,7 +49,8 @@ class SinglePostView(View):
         context = {
           "post": post,
           "post_tags": post.tags.all(),
-          "comment_form": CommentForm()
+          "comment_form": CommentForm(),
+          "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -67,6 +68,7 @@ class SinglePostView(View):
         context = {
           "post": post,
           "post_tags": post.tags.all(),
-          "comment_form": comment_form
+          "comment_form": comment_form,
+          "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
