bab60fbef28abe0e473ba3ecdfa70eb13379abef
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-08-24 01:03:31 +0400

    Comment Form Validation Styles

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ["post"]
-        label = {
+        labels = {
             "user_name": "Your Name",
             "user_email": "Your Email",
             "text": "Your Comment"
